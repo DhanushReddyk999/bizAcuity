45f9aabeb8836f4d68e1493418d0651c1c146336
DhanushReddyk999 <komatireddydhanushreddy@gmail.com>
2025-08-01 13:02:21 +0530

    Fix bcrypt import to use bcryptjs in mail-verification.js

--- virtual_wall/backend/routes/mail-verification.js
+++ virtual_wall/backend/routes/mail-verification.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const { sendWelcomeEmail, sendPasswordResetEmail } = require('../notification');
 const { validatePassword } = require('../utils/passwordValidation');
